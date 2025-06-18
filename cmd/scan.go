9796f2ee29ec3b05e4633e3fc2f09e84214a5b04
Filipe Pina <63779195+fopinappb@users.noreply.github.com>
2025-06-18 10:22:14 +0100

    x

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,14 +1,23 @@
+// based off https://github.com/go-ble/ble/blob/master/examples/basic/scanner/main.go
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"strings"
+	"time"
 
+	"github.com/go-ble/ble"
+	"github.com/go-ble/ble/examples/lib/dev"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 type scanOptions struct {
-	multiply bool
-	add      bool
+	device     string
+	duration   time.Duration
+	duplicates bool
 }
 
 func defaultScanOptions() *scanOptions {
@@ -25,14 +34,61 @@ func newScanCmd() *cobra.Command {
 		RunE:         o.run,
 	}
 
-	cmd.Flags().BoolVarP(&o.multiply, "multiply", "m", o.multiply, "multiply")
-	cmd.Flags().BoolVarP(&o.add, "add", "a", o.add, "add")
+	cmd.Flags().StringVarP(&o.device, "device", "d", "default", "implementation of ble")
+	cmd.Flags().DurationVarP(&o.duration, "duration", "w", 5*time.Second, "scanning duration")
+	cmd.Flags().BoolVarP(&o.duplicates, "dup", "", true, "allow duplicate reported")
 
 	return cmd
 }
 
 func (o *scanOptions) run(cmd *cobra.Command, args []string) error {
-	fmt.Println("oi")
+	d, err := dev.NewDevice(o.device)
+	if err != nil {
+		log.Fatalf("can't new device : %s", err)
+	}
+	ble.SetDefaultDevice(d)
+
+	// Scan for specified durantion, or until interrupted by user.
+	fmt.Printf("Scanning for %s...\n", o.duration)
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), o.duration))
+	chkErr(ble.Scan(ctx, o.duplicates, advHandler, nil))
 
 	return nil
+}
+
+func advHandler(a ble.Advertisement) {
+	if !strings.HasPrefix(a.LocalName(), "LEDnetWF") {
+		return
+	}
+
+	if a.Connectable() {
+		fmt.Printf("[%s] C %3d:", a.Addr(), a.RSSI())
+	} else {
+		fmt.Printf("[%s] N %3d:", a.Addr(), a.RSSI())
+	}
+	comma := ""
+	if len(a.LocalName()) > 0 {
+		fmt.Printf(" Name: %s", a.LocalName())
+		comma = ","
+	}
+	if len(a.Services()) > 0 {
+		fmt.Printf("%s Svcs: %v", comma, a.Services())
+		comma = ","
+	}
+	if len(a.ManufacturerData()) > 0 {
+		fmt.Printf("%s MD: %X", comma, a.ManufacturerData())
+	}
+	fmt.Printf("\n")
+}
+
+func chkErr(err error) {
+	switch errors.Cause(err) {
+	case nil:
+	case context.DeadlineExceeded:
+		fmt.Printf("done\n")
+	case context.Canceled:
+		fmt.Printf("canceled\n")
+	default:
+		log.Fatalf(err.Error())
+	}
 }
--- go.mod
+++ go.mod
@@ -8,12 +8,23 @@ require (
 )
 
 require (
+	github.com/JuulLabs-OSS/cbgo v0.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-ble/ble v0.0.0-20240122180141-8c5522f54333 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/mgutz/logxi v0.0.0-20161027140823-aebf8a7d67ab // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/raff/goble v0.0.0-20190909174656-72afc67d6a99 // indirect
+	github.com/sirupsen/logrus v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211204120058-94396e421777 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
