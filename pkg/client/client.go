4b2f2af107dbc75ed70f111872ca054fea72b5dd
Filipe Pina <636320+fopina@users.noreply.github.com>
2025-06-23 17:45:38 +0200

    move code to its own client package (#6)

---
+++ pkg/client/client.go
@@ -0,0 +1,109 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/fopina/zengge-led-ctl/pkg/dev"
+	"github.com/go-ble/ble"
+	"github.com/pkg/errors"
+)
+
+type ZenggeClient struct {
+	deviceName string
+	device     ble.Device
+	client     ble.Client
+	notifyChar *ble.Characteristic
+	writeChar  *ble.Characteristic
+}
+
+func NewZenggeClient(device string) (*ZenggeClient, error) {
+	d, err := dev.NewDevice(device)
+	if err != nil {
+		return nil, fmt.Errorf("can't new device : %s", err)
+	}
+	ble.SetDefaultDevice(d)
+	return &ZenggeClient{
+		deviceName: device,
+		device:     d,
+	}, nil
+}
+
+func (c *ZenggeClient) Scan(duration time.Duration, duplicates bool, handler ble.AdvHandler) error {
+	scanHandler := func(a ble.Advertisement) {
+		if !strings.HasPrefix(a.LocalName(), "LEDnetWF") {
+			return
+		}
+		handler(a)
+	}
+
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), duration))
+	return chkScanErr(ble.Scan(ctx, duplicates, scanHandler, nil))
+}
+
+func (c *ZenggeClient) Connect(addr string, duration time.Duration) error {
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), duration))
+	// Dial directly, skip Connect - it's slower (as it scans) and DOESN'T EVEN WORK!... https://github.com/go-ble/ble/pull/112
+	client, err := ble.Dial(ctx, ble.NewAddr(addr))
+	if err != nil {
+		return err
+	}
+	c.client = client
+
+	_, err = client.DiscoverProfile(true)
+	if err != nil {
+		return fmt.Errorf("can't discover profile: %s", err)
+	}
+
+	notify := client.Profile().FindCharacteristic(ble.NewCharacteristic(ble.MustParse(NotifyUUID)))
+	if notify == nil {
+		return fmt.Errorf("cannot find characteristic to subscribe")
+	}
+	c.notifyChar = notify
+
+	write := client.Profile().FindCharacteristic(ble.NewCharacteristic(ble.MustParse(WriteUUID)))
+	if write == nil {
+		return fmt.Errorf("cannot find characteristic to write")
+	}
+	c.writeChar = write
+	return nil
+}
+
+func (c *ZenggeClient) Subscribe(handler ble.NotificationHandler) error {
+	notificationhandler := func(req []byte) {
+		// TODO: implement parsing
+		handler(req)
+	}
+
+	return c.client.Subscribe(c.notifyChar, false, notificationhandler)
+}
+
+// SendInitialPackage ??? TBD what this is...
+func (c *ZenggeClient) SendInitialPacket() error {
+	return c.client.WriteCharacteristic(c.writeChar, InitialPacket, false)
+}
+
+// GetStripSettings ??? TBD what this is...
+func (c *ZenggeClient) GetStripSettings() error {
+	return c.client.WriteCharacteristic(c.writeChar, GetStripSettingsPacket, false)
+}
+
+// PowerOff Power off the LED strip
+func (c *ZenggeClient) PowerOff() error {
+	return c.client.WriteCharacteristic(c.writeChar, PowerOffPacket, false)
+}
+
+func chkScanErr(err error) error {
+	switch errors.Cause(err) {
+	case nil:
+	case context.DeadlineExceeded:
+		fmt.Printf("done\n")
+	case context.Canceled:
+		fmt.Printf("canceled\n")
+	default:
+		return err
+	}
+	return nil
+}
